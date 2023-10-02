fcade74f8488e1d248a4ceec3c7e1dc40f8294f0
Marcielle Legarde <marciellelegarde@wireless-10-155-247-12.public.utexas.edu>
2023-10-02 11:21:13 -0500

    master.py updates

--- Project/master.py
+++ Project/master.py
@@ -18,6 +18,23 @@ num_intersections = 0        # increments according to number of intersections
 leftMotor=int(100)
 rightMotor=int(100)
 
+
+# set initial values
+x = -1 # left motor reading
+y = -1 # right motor reading
+z = -1 # line sensor reading
+
+# bump sensors 
+#a bump sensor that is unactivated starts at 1 (because they are pullups), hence why these are all one
+FR=int(1)   # far right
+R=int(1)    # right
+MR=int(1)   # middle right
+ML=int(1)   # middle left
+L=int(1)    # left
+FL=int(1)   # far left
+BUMPS = [0,0,0,0,0,0]   # array of values, will not be 1 if has been pushed
+
+
 def bumpSensors():
     return
 
@@ -40,18 +57,30 @@ if __name__ == '__main__':
         
         if ser.in_waiting > 0:  #we wait until the arduino has sent something to us before we try to read anything from the serial port.
             
-            line = ser.readline().decode('utf-8')
-            # print(line)
+            line = ser.readline().decode('utf-8') # read incoming string
+            # print(line) # debug
 
-            #this splits the incoming string up by commas
-            line=line.split(',')
+            line=line.split(',') # split incoming string into list with comma delimeter
             try:
-                x = int(line[0])
-                y = int(line[1])
-                z = int(line[2]) # we dont convert this to a float becasue we went to be able to recieve the message that we are at a cross, which wont be an int. 
-                # print([x,y,z])
-                # print(now)
-                # print(old)
+                # line following 
+                x = int(line[0]) # left motor
+                y = int(line[1]) # right motor
+                z = int(line[2]) # line detection
+                
+                # bump sensors
+                FR=int(line[3]) # far right
+                R=int(line[4])  # right
+                MR=int(line[5]) # middle right
+
+                ML=int(line[6]) # middle left
+                L=int(line[7])  # left
+                FL=int(line[8]) # far left
+
+                BUMPS = [FL, L, ML, MR, R, FR] # array version of bump sensors from left to right
+                # print('BUMPS: ', BUMPS) # debug
+                # print('x, y, z: ', [x,y,z]) # debug
+                # print('now: ', now) # debug
+                # print('old: ', old) # debug
                 
             except:
                 print("packet dropped") # this is designed to catch when python shoves bits on top of each other. 
@@ -59,19 +88,23 @@ if __name__ == '__main__':
 
             if (now - old) > .0001: # check if time stamp differs by interval
                 old = now # update timestamp for last iteration of this if
+                
+                # if bump sensor has been pressed, run bump function.... but is this blocking?
+                if BUMPS != [0,0,0,0,0,0]: 
+                    bumpSensors(BUMPS)
+                    
+                elif (z != -1):
+                    lineFollowing(z)
+                    break
+                
                 if z == 0:
                     print('ERROR: DETECT NO LINE????')
                     ### REVISIT THIS SECTION ### will need to prepare for this case. seek line?
                     
                 elif not z < 7000: #im assuming that in your arduino code you will be setting z to the int 8000 if you sense a cross, dont feel obligated to do it this way.  
-                    # now that we are SURE that z isnt the string cross, we cast z to an int and recalculate leftMotor and rightMotor, 
-                    ## move left (not pivot in place)
-                    # do something here like incrimenting a value you call 'lines_hit' to one higher, and writing code to make sure that some time (1 second should do it) 
-                    # passes between being able to incriment lines_hit so that it wont be incrimented a bunch of times when you hit your first cross. IE give your robot time to leave a cross
-                    # before allowing lines_hit to be incrimented again.
                     print('AT INTERSECTION')
-                    
                     cross_time_new = time.time() # constantly reassign time for checking intersection
+                    
                     if (cross_time_new - cross_time_old) > 1: # check if time elapsed since last intersection > 1 SECOND
                         cross_time_old = cross_time_new # reassign for time of last intersection
                         num_intersections +=1 # increment number of intersections
