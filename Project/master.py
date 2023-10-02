feb1c302579294694a6ca5fa9338a08e4000e231
Marcielle Legarde <marciellelegarde@wireless-10-155-247-12.public.utexas.edu>
2023-10-02 11:07:58 -0500

    master.py updates

--- Project/master.py
+++ Project/master.py
@@ -18,6 +18,14 @@ num_intersections = 0        # increments according to number of intersections
 leftMotor=int(100)
 rightMotor=int(100)
 
+def bumpSensors():
+    return
+
+def irSensors():
+    return
+
+def lineFollowing():
+    return
 
 
 ##########
@@ -31,43 +39,26 @@ if __name__ == '__main__':
         now = time.time() # constantly reassign new timestamp
         
         if ser.in_waiting > 0:  #we wait until the arduino has sent something to us before we try to read anything from the serial port.
-            #### remove below (debug) ####
-
-            leftMotor +=1
-            rightMotor +=1
             
-            if leftMotor > 400 or rightMotor > 400:
-                break
-
-            #sendString(port,115200,'<'+str(leftMotor)+','+str(rightMotor)+'>',0.0001)
-            #### remove above (debug) ####
-
-
             line = ser.readline().decode('utf-8')
-            line=line.split(',')
-            print(line)
+            # print(line)
+
             #this splits the incoming string up by commas
+            line=line.split(',')
             try:
-                
                 x = int(line[0])
                 y = int(line[1])
-                z = int(line[2]) #we dont convert this to a float becasue we went to be able to recieve the message that we are at a cross, which wont be an int. 
-                print([x,y,z])
+                z = int(line[2]) # we dont convert this to a float becasue we went to be able to recieve the message that we are at a cross, which wont be an int. 
+                # print([x,y,z])
                 # print(now)
                 # print(old)
+                
             except:
-                print("packet dropped") #this is designed to catch when python shoves bits on top of each other. 
+                print("packet dropped") # this is designed to catch when python shoves bits on top of each other. 
 
 
-        
-        
-            #Following is my control law, we're keeping it basic for now, writing good control law is your job
-            #ok so high numbers(highest 7000) on the line follwing mean I am too far to the LEFT,
-            #low numbers mean I am too far on the RIGHT, 3500 means I am at the middle
-            #below is a basic control law you can send to your motors, with an exeption if z is a value greater than 7000, meaning the arduino code sees that the line sensor is on a cross. Feel free to take insperation from this,
-            #but you will need to impliment a state machine similar to what you made in lab 2 (including a way of counting time without blocking)
             if (now - old) > .0001: # check if time stamp differs by interval
-                old = now ## update timestamp for last iteration of this if
+                old = now # update timestamp for last iteration of this if
                 if z == 0:
                     print('ERROR: DETECT NO LINE????')
                     ### REVISIT THIS SECTION ### will need to prepare for this case. seek line?
